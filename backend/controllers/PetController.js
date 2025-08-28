328e1c3429085ce2ff51ccf5d50211e72b503165
HalfLauren17 <limac7503@gmail.com>
2025-08-27 22:18:52 -0300

    controllers & routes

---
+++ backend/controllers/PetController.js
@@ -0,0 +1,9 @@
+const Pet = require("../models/Pet");
+
+module.exports = class PetController{
+
+    static async register(req, res){
+        res.json("Teste");
+    }
+
+}
--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,16 @@ const app = express();
 app.use(express.json());
 
 //Solve CORS
-app.use({ credentials: true, origin: "http://localhost:3000" });
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 //Public images folder
 app.use(express.static("./public"));
 
 //Routes
+const UserRoutes = require("./routes/UserRoutes");
+const PetRoutes = require("./routes/PetRoutes");
+
+app.use("/users", UserRoutes);
+app.use("/pets", PetRoutes);
 
 app.listen(5000);
