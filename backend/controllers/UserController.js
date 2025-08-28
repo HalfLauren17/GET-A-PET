2e59f493fe8e31456478338f02e5f0e4d577d756
HalfLauren17 <limac7503@gmail.com>
2025-08-27 22:47:10 -0300

    User register validations

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,9 +1,45 @@
 const User = require("../models/User");
 
-module.exports = class UserController{
+module.exports = class UserController {
+  static async register(req, res) {
+    const { name, email, password, confirmPassword, image, phone } = req.body;
 
-    static async register(req, res){
-        res.json("Teste");
+    //Validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório." });
+      return;
+    }
+    if (!email) {
+      res.status(422).json({ message: "O email é obrigatório." });
+      return;
+    }
+    if (!password) {
+      res.status(422).json({ message: "O senha é obrigatório." });
+      return;
+    }
+    if (!confirmPassword) {
+      res
+        .status(422)
+        .json({ message: "A confirmação de senha é obrigatória." });
+      return;
+    }
+    if (!phone) {
+      res.status(422).json({ message: "O telefone é obrigatório." });
+      return;
+    }
+    if (password !== confirmPassword) {
+      res.status(422).json({
+        message: "A senha e a confirmação de senha precisam ser iguais.",
+      });
+      return;
+    }
+    //Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(422).json({ message: "Por favor, utilize outro e-mail." });
+      return;
     }
 
-}
+    res.status(200).json({ message: "OK" });
+  }
+};
