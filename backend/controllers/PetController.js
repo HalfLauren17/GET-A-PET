686af4709a69b9b303f579b4b25fdde0564ed095
HalfLauren17 <limac7503@gmail.com>
2025-08-29 23:10:19 -0300

    Create pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,9 +1,71 @@
 const Pet = require("../models/Pet");
 
-module.exports = class PetController{
+//Helpers
+const getToken = require("../helpers/get-token");
+const getUserByToken = require("../helpers/get-user-by-token");
 
-    static async register(req, res){
-        res.json("Teste");
+module.exports = class PetController {
+  static async register(req, res) {
+    const { name, age, weight, color } = req.body;
+
+    const images = req.files;
+
+    const available = true;
+
+    //Images upload
+
+    //Validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório." });
+      return;
+    }
+    if (!age) {
+      res.status(422).json({ message: "A idade é obrigatória." });
+      return;
+    }
+    if (!weight) {
+      res.status(422).json({ message: "O peso é obrigatório." });
+      return;
+    }
+    if (!color) {
+      res.status(422).json({ message: "A cor é obrigatória." });
+      return;
+    }
+    if (images.length === 0) {
+      res.status(422).json({ message: "A imagem é obrigatória." });
+      return;
     }
 
-}
+    //Get pet owner
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    //Create pet
+    const pet = new Pet({
+      name,
+      age,
+      weight,
+      color,
+      images: [],
+      available,
+      user: {
+        _id: user.id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      },
+    });
+
+    images.map((image) => {
+        pet.images.push(image.filename);
+    });
+
+    try {
+      const newPet = await pet.save();
+
+      res.status(201).json({ message: "Pet cadastrado com sucesso.", newPet });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+};
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const PetController = require("../controllers/PetController");
 
-router.post("/register", PetController.register);
+//Middleware
+const verifyToken = require("../helpers/verify-token");
+const { imageUpload } = require("../helpers/image-upload");
+
+router.post(
+  "/register",
+  verifyToken,
+  imageUpload.array("images"),
+  PetController.register
+);
 
 module.exports = router;
