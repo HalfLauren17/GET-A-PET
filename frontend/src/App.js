1da80035304dc3b1b33389fb93f8b8bd54dee04f
HalfLauren17 <limac7503@gmail.com>
2025-09-09 18:09:09 -0300

    Frontend layout

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Pages
+import Home from "./components/pages/Home";
+import Register from "./components/pages/auth/Register";
+import Login from "./components/pages/auth/Login";
+
+//Components
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Container from "./components/layouts/Container"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </Container>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layouts/Navbar.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+import styles from "./Navbar.module.css";
+
+import Logo from "../../assets/img/logo.png";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarLogo}>
+        <img src={Logo} alt="Logo do site" />
+        <h2>Get A Pet</h2>
+      </div>
+      <ul>
+        <li>
+          <Link to="/"> Adotar </Link>
+        </li>
+        <li>
+          <Link to="/login"> Entrar </Link>
+        </li>
+        <li>
+          <Link to="/register"> Cadastrar </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
