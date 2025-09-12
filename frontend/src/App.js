05bef359603af599a681868ab44d4ec214d4eae5
HalfLauren17 <limac7503@gmail.com>
2025-09-11 21:47:27 -0300

    Register user form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 //Pages
 import Home from "./components/pages/Home";
--- frontend/src/components/pages/auth/Register.js
+++ frontend/src/components/pages/auth/Register.js
@@ -1,7 +1,55 @@
+import Input from "../../form/Input";
+import {Link} from "react-router";
+
+import styles from "../../form/Form.module.css";
+
 export default function Register() {
+  function handleChange(e) {}
+
   return (
-    <section>
-      <h1>Register</h1>
+    <section className={styles.formContainer}>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text="Nome:"
+          type="text"
+          name="name"
+          placeholder="Digite seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Telefone:"
+          type="text"
+          name="phone"
+          placeholder="Digite seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="E-mail:"
+          type="email"
+          name="email"
+          placeholder="Digite seu e-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha:"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha:"
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta?<Link to="/login">Clique aqui!</Link>
+      </p>
     </section>
   );
 }
